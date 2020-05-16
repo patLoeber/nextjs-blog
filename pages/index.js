41f59b077ec38e42aa8ac378534e2d8e08ae3934
Patrick Löber <patrickloeber90@googlemail.com>
2020-05-16 08:12:20 +0200

    added new hero description

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,13 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>…</section>
+      <section className={utilStyles.headingMd}>
+        <p>Hi, I'm Pat. A Software Engineer.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
